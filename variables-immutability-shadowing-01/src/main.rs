d3c9db48a235a419afcec4100c70ffbf40ae0cbd
Stephen Ekeh <stevenadibee@yahoo.com>
2026-06-28 20:31:08 +0100

    Variable-immutability-shadowing-01

---
+++ variables-immutability-shadowing-01/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    // Variable creation and initailization 
+    let name = "Steven";
+    println!("Name: {}", name);
+    
+
+    //Variables are immutable unless specified mutable 
+    let mut age = 25;
+    println!("Age before update: {}", age);
+    
+    //the value assigned to age can be changed because it is specified mutable
+    age = 26;
+    println!("Age after update: {}", age);
+
+    //Demonstrating shadowing 
+    let email = "steve.example.com";
+    //using the same variable name shadows the first one
+    let email = "ekeh.example.com";
+    println!("{}", email);
+}
